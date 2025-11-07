6c482f5ca342e517bc3a9a204bc91f04e93b69db
Yosorable <yosora2016@gmail.com>
2025-11-07 19:17:07 +0800

    update platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ECMASwift",
-    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)],
+    platforms: [.macOS(.v11), .iOS(.v15), .tvOS(.v15), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
